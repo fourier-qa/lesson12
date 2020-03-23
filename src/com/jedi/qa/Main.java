cebff415441c758cd5e93df5d696e340f7ad97de
admin <admin@travel-carbon03.ad.mirantis.net>
2020-03-23 20:32:30 +0400

    Initial commit: added Main.java

---
+++ src/com/jedi/qa/Main.java
@@ -0,0 +1,20 @@
+package com.jedi.qa;
+
+import java.io.IOException;
+
+public class Main {
+    public static voids functionToPrintNumber()
+    {
+        int number = 6;
+        System.out.println("Number: " + number);
+    }
+
+    public static void main(String[] args) throws IOException {
+        int a = 5;
+        int b = 2;
+
+        int c = a * 2 + b;
+
+        functionToPrintNumber();
+    }
+}
